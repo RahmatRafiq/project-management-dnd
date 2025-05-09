d45c27a6c127f127062f132dd5cc4750bf631332
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-05-09 23:59:52 +0800

    Refactor TaskUser model to remove unnecessary comments and clarify table definition

--- app/Models/taskUser.php
+++ app/Models/taskUser.php
@@ -5,6 +5,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class TaskUser extends Model
 {
-    protected $table   = 'task_user'; // Specify the table name
-    public $timestamps = false;       // Disable timestamps if not needed
+    protected $table = 'task_user';
 }
