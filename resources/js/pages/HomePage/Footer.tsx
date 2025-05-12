33ca4fa42ce52c06d7a7fa7bf5a6dbf6b3867770
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-05-12 22:00:31 +0800

    Add Footer component with copyright notice and styling

---
+++ resources/js/pages/HomePage/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-900 text-gray-400 py-10">
+            <div className="container mx-auto text-center">
+                <p>© 2025 NotionFlow. All Rights Reserved.</p>
+            </div>
+        </footer>
+    );
+}
