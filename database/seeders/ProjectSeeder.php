c9bcf912dfd9fb3faf4175dbb378e7c1e3441770
RahmatRafiq <rahmatrafiq.1999@gmail.com>
2025-06-01 22:08:52 +0800

    Refactor ProjectSeeder and UserSeeder: Enhance user creation with roles and add task and comment seeding in ProjectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,23 +3,52 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Project;
+use App\Models\Task;
+use App\Models\Comment;
+use App\Models\User;
+use Illuminate\Support\Arr;
 
 class ProjectSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
         $faker = \Faker\Factory::create();
 
-        foreach (range(1, 50) as $index) {
-            Project::create([
+        if (User::count() < 5) {
+            User::factory()->count(5)->create();
+        }
+        $users = User::all();
+
+        foreach (range(1, 10) as $i) {
+            $project = Project::create([
                 'name'        => $faker->words(3, true),
                 'description' => $faker->sentence(),
                 'metadata'    => ['key' => $faker->word],
                 'is_active'   => $faker->boolean(),
             ]);
+
+            foreach (range(1, rand(2, 5)) as $j) {
+                $task = Task::create([
+                    'project_id' => $project->id,
+                    'title'      => $faker->sentence(3),
+                    'details'    => $faker->paragraph(),
+                    'start_date' => $faker->dateTimeBetween('-1 month', 'now'),
+                    'end_date'   => $faker->dateTimeBetween('now', '+1 month'),
+                    'done'       => $faker->boolean(),
+                    'tags'       => $faker->words(2),
+                ]);
+
+                $assignedUsers = $users->random(rand(1, 3));
+                $task->users()->attach($assignedUsers->pluck('id'));
+
+                foreach (range(1, rand(1, 3)) as $k) {
+                    Comment::create([
+                        'task_id' => $task->id,
+                        'user_id' => $assignedUsers->random()->id,
+                        'body'    => $faker->sentence(),
+                    ]);
+                }
+            }
         }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Database\Seeders;
 
 use App\Models\User;
@@ -10,11 +9,35 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        // Create roles first
+        $adminRole         = Role::firstOrCreate(['name' => 'admin']);
+        $administratorRole = Role::firstOrCreate(['name' => 'administrator']);
+
+        // Create users and assign roles
+        $user1 = User::create([
+            'name'     => 'User 1',
+            'email'    => 'user1@gmail.com',
+            'password' => bcrypt('user1@gmail.com'),
+        ]);
+        $user1->assignRole($adminRole);
+
+        $user2 = User::create([
+            'name'     => 'User 2',
+            'email'    => 'user2@gmail.com',
+            'password' => bcrypt('user2@gmail.com'),
+        ]);
+        $user2->assignRole($administratorRole);
+
         $roles = Role::all();
 
-        User::factory(100)->create()->each(function ($user) use ($roles) {
-            // Assign random role
+        // Generate 20 users with Faker and assign random roles
+        User::factory(20)->create()->each(function ($user) use ($roles) {
             $user->assignRole($roles->random());
         });
+
+        // Optionally, generate 100 more users if needed
+        // User::factory(100)->create()->each(function ($user) use ($roles) {
+        //     $user->assignRole($roles->random());
+        // });
     }
 }
